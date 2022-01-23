1c25b87b80a82965a53dc2247fdc03e4f82624d6
CR4CKID <rlarl1999@gmail.com>
2022-01-23 17:44:51 +0900

    Bio

--- practice.py
+++ practice.py
@@ -1,3 +1,14 @@
-import Bio
+s = "GATATATGCATATACTT"
+t = "ATAT"
 
-print(Bio.__version__)
+answer = []
+start_idx = 0
+
+try:
+    while True:
+        idx = s.index(t, start_idx)
+        answer.append(idx + 1)
+        start_idx = idx + 1
+except:
+    print(" ".join(map(str, answer)))
+
