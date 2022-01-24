deb690fe9433d1a1b165a67a4c6b42904ee1e298
CR4CKID <rlarl1999@gmail.com>
2022-01-25 04:39:56 +0900

    Bio

--- practice.py
+++ practice.py
@@ -1,3 +1,17 @@
-arr = [[1, 2, 3], ["a", "b", "c"]]
+month, life = 17, 5
+answer = [0] * (month + 1)
+dying = [0] * (month + life + 1)
 
-print(list(zip(*arr)))
+
+def mortal_rab(month):
+    if month <= 2:
+        dying[life + 1] = 1
+        answer[month] = 1
+    if answer[month] == 0:
+        answer[month] = mortal_rab(month - 1) + mortal_rab(month - 2) - dying[month]
+        dying[month + life - 1] = mortal_rab(month - 2)
+    return answer[month]
+
+
+mortal_rab(month)
+print(answer)
