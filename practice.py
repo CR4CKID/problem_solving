3933ad7ae227d87187cbe94b0c34923bcc58a874
CR4CKID <rlarl1999@gmail.com>
2022-02-05 23:01:56 +0900

    22/02/05

--- practice.py
+++ practice.py
@@ -1,9 +1,7 @@
 from Bio.Align.Applications import MuscleCommandline
 
-muscle_exe = r"C:\Users\CRACKID\Desktop\Util\muscle.exe"
-cmd_line = MuscleCommandline(muscle_exe, input="HBA.all.fasta", out="HBA.aln", clw="")
-
+muscle_exe = r"C:\Users\CRACKID\Desktop\Util\muscle_v3.exe"
+cmd_line = MuscleCommandline(muscle_exe, input="HBA.all.fasta", out="HBA.aln", clw=True)
 print(cmd_line)
 
-cmd_line()
-
+stdout, stderr = cmd_line()
