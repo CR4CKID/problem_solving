fcef780f6943405988f766c7ccb89e06d1687d96
CR4CKID <rlarl1999@gmail.com>
2022-02-05 20:51:40 +0900

    22/02/05

--- practice.py
+++ practice.py
@@ -1,18 +1,9 @@
-month, life = 17, 5
-answer = [0] * (month + 1)
-dying = [0] * (month + life + 1)
+from Bio.Align.Applications import MuscleCommandline
 
+muscle_exe = r"C:\Users\CRACKID\Desktop\Util\muscle.exe"
+cmd_line = MuscleCommandline(muscle_exe, input="HBA.all.fasta", out="HBA.aln", clw="")
 
-def mortal_rab(month):
-    if month <= 2:
-        dying[life + 1] = 1
-        answer[month] = 1
-    if answer[month] == 0:
-        answer[month] = mortal_rab(month - 1) + mortal_rab(month - 2) - dying[month]
-        dying[month + life - 1] = mortal_rab(month - 2)
-    return answer[month]
+print(cmd_line)
 
+cmd_line()
 
-mortal_rab(month)
-print(answer)
-asd
