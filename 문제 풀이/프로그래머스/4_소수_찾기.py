e93a42881304798a57473b2b37e956d811db71b6
CR4CKID <rlarl1999@gmail.com>
2022-02-10 18:54:09 +0900

    22/02/10

--- 문제 풀이/프로그래머스/4_소수_찾기.py
+++ 문제 풀이/프로그래머스/4_소수_찾기.py
@@ -11,9 +11,38 @@
                 for문을 사용하더라도 여러자리의 숫자를 어캐 만들까
                     함수 내에 함수를 넣어서 하면 중복을 피할 수가 없음
                     
+            itertools.permutations으로 해결
+                    
         2. 섞어 만든 숫자가 소수인지를 확인하는 파트
         
     11과 011은 같은 숫자로 취급하므로 str을 int로 바꿔주기
     
 """
+from itertools import permutations
+
+
+def solution(numbers):
+    arr = [n for n in numbers]
+    prm = set()
+    ans = 0
+
+    def under_pri(n):
+        if n <= 1:
+            return 0
+        for i in range(2, int(n ** 0.5 + 1)):
+            if n % i == 0:
+                return 0
+        else:
+            return 1
+
+    for r in range(len(arr)):
+        prm.update(set(map(int, map("".join, permutations(arr, r + 1)))))
+
+    for n in prm:
+        ans += under_pri(n)
+
+    return ans
+
+
+print(solution("011"))
 
