f5878af3b60f5645a2207cd96e7832c36a12d938
CR4CKID <rlarl1999@gmail.com>
2022-02-10 14:10:49 +0900

    22/02/10

--- practice2.py
+++ practice2.py
@@ -1,2 +1,17 @@
-list2 = [122,3,3]
-list2.sort
+fin = []
+for i1 in range(1, 4):
+    ans = []
+    ans.append(i1)
+    for i2 in range(1, 4):
+        if i2 in ans:
+            continue
+        else:
+            ans.append(i2)
+            for i3 in range(1, 4):
+                if i3 in ans:
+                    continue
+                else:
+                    ans.append(i3)
+                    fin.append(ans)
+
+print(fin)
