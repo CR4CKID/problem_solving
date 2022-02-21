c29d4ba4634ea559cee3c7ef27e70820a3d80672
CR4CKID <rlarl1999@gmail.com>
2022-02-22 02:36:40 +0900

    22/02/22

--- practice.py
+++ practice.py
@@ -1,8 +1,26 @@
-arr = [1112, 695]
+from Bio import SeqIO
 
-while max(arr) % min(arr) != 0:
-    arr.append(max(arr) % min(arr))
-    arr.pop(0)
+records = SeqIO.parse("sample.fasta", "fasta")
+data = [str(record.seq) for record in records]
 
-print(arr[-1])
 
+def substring(long, short):
+    l = len(short) // 2
+
+    def combine(a, b):
+        return a[: a.index(b[:l])] + b
+
+    if short[:l] in long:
+        return combine(long, short)
+    elif long[:l] in short:
+        return combine(short, long)
+
+
+ans = data.pop()
+while data:
+    for seq1 in data:
+        if substring(ans, seq1):
+            ans = substring(ans, seq1)
+            data.remove(seq1)
+            break
+print(ans)
